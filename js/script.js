165cd4d6b89509665aeeec7779d88c1b8d972dbf
ayobanwo <olubanwoayomide@gmail.com>
2022-08-17 04:21:18 +0100

    new commit

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 
 var typed = new Typed(".typing-1", {
-    strings: ["" ,"An Army of young people ", "Between the age of 5years to 17years", "Changing the world for Jesus," , "One Light / Life at a time."],
+    strings: ["" ,"An Army of young people ", "Between the age of 5years to 17years", "Changing the world for Jesus." , "One Light / Life at a time."],
     typeSpeed: 100,
     BackSpeed: 60,
     loop:true
